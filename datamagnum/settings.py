5c2745c2528287c251334bd51daa43ed6bfcf9d6
Luis <deathghoul41@gmail.com>
2023-04-07 18:24:05 +0200

    digitalocean

--- datamagnum/settings.py
+++ datamagnum/settings.py
@@ -25,9 +25,9 @@ SECURE_SSL_REDIRECT = False
 SECRET_KEY = 'yx24%_&s-nvd6jcr@b8vk%le1r^-%$7^t&z&@q9qa9yz--y-v1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://datamagnum.fly.dev', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 CSRF_TRUSTED_ORIGINS = ['https://datamagnum.fly.dev']
 # production
 if ENVIRONMENT == 'production':
