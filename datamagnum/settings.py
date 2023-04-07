c74c4e7f7c27b16e2e891dbfef771b472f9e6871
Luis <deathghoul41@gmail.com>
2023-04-07 20:27:33 +0200

    fix

--- datamagnum/settings.py
+++ datamagnum/settings.py
@@ -133,16 +133,11 @@ WSGI_APPLICATION = 'datamagnum.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'datamagnum',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': 5432
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
