17faf6620a8f1f0b063abb7abe10139e113ee07d
Luis <deathghoul41@gmail.com>
2023-04-10 23:47:30 +0200

    fix

--- railway.sh
+++ railway.sh
@@ -1,3 +1,2 @@
-python manage.py migrate & gunicorn datamagnum.wsgi & wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &
-sudo dpkg -i google-chrome-stable_current_amd64.deb & sudo apt-get install -f & pip install -U "celery[redis]" &
+python manage.py migrate & gunicorn datamagnum.wsgi & 
 redis-cli & celery -A datamagnum worker --pool=solo -l INFO & celery -A datamagnum beat -l INFO 
