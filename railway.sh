c397893714afc1385145e334e2699c7c3ee47082
Luis <deathghoul41@gmail.com>
2023-04-10 19:08:44 +0200

    fix

--- railway.sh
+++ railway.sh
@@ -1,2 +1,2 @@
-python manage.py migrate & gunicorn datamagnum.wsgi & celery -A datamagnum worker --pool=solo -l INFO &
-celery -A datamagnum beat -l INFO & stripe listen --forward-to https://web-production-b540.up.railway.app/stripe-webhook-paid/ 
+python manage.py migrate & gunicorn datamagnum.wsgi & celery -A datamagnum worker --uid 1000 --pool=solo -l INFO &
+celery -A datamagnum beat -l --uid 1000 INFO 
