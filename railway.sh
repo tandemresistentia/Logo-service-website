4c413e6e3c1330486c5da6e5a3bf7619bd07d304
Luis <deathghoul41@gmail.com>
2023-04-10 19:13:55 +0200

    fix

--- railway.sh
+++ railway.sh
@@ -1,2 +1,2 @@
 python manage.py migrate & gunicorn datamagnum.wsgi & celery -A datamagnum worker --uid 1000 --pool=solo -l INFO &
-celery -A datamagnum beat -l --uid 1000 INFO 
+celery -A datamagnum beat -l INFO 
