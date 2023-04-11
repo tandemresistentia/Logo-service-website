091a184d6d2df2d808f0bbc8a176ee5810e246f7
Luis <deathghoul41@gmail.com>
2023-04-11 15:29:22 +0200

    fix

--- railway.sh
+++ railway.sh
@@ -1,2 +1,2 @@
-python manage.py migrate & gunicorn datamagnum.wsgi & sudo apt install -y chromium-browser &
+python manage.py migrate & gunicorn datamagnum.wsgi &
 redis-cli & celery -A datamagnum worker --pool=solo -l INFO & celery -A datamagnum beat -l INFO 
