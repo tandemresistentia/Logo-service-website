1e77c5060e2e6ceed07a1dda114a584cf5e11ab1
Luis <deathghoul41@gmail.com>
2023-04-11 16:39:40 +0200

    fix

--- railway.sh
+++ railway.sh
@@ -1,4 +1,4 @@
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &
-sudo apt-get update & sudo apt-get install libappindicator1 & sudo dpkg -i google-chrome-stable_current_amd64.deb &
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+sudo apt-get update && sudo apt-get install libappindicator1 && sudo dpkg -i google-chrome-stable_current_amd64.deb &&
 python manage.py migrate & gunicorn datamagnum.wsgi &
 redis-cli & celery -A datamagnum worker --pool=solo -l INFO & celery -A datamagnum beat -l INFO 
