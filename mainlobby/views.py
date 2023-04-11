3a59f3b04abb2c1902b24f0a3ce04d03d3cb8a37
Luis <deathghoul41@gmail.com>
2023-04-12 00:01:08 +0200

    fix

--- mainlobby/views.py
+++ mainlobby/views.py
@@ -313,6 +313,8 @@ class Browser:
             self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         options = uc.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox') 
+        options.add_argument('--disable-dev-shm-usage')    
     # options.add_argument("--user-agent=" + self.user_agent)
         self.bot = uc.Chrome(options=options)
         self.bot.delete_all_cookies()
