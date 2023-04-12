a6439f4fdc73bb01b3ad214104c90a0a2d612024
Luis <deathghoul41@gmail.com>
2023-04-12 16:34:19 +0200

    fix

--- mainlobby/tasks.py
+++ mainlobby/tasks.py
@@ -17,6 +17,80 @@ from selenium.webdriver.common.keys import Keys
 import undetected_chromedriver as uc
 from fake_useragent import UserAgent, FakeUserAgentError
 from django.http import HttpRequest
+import os 
+import shutil
+import tempfile
+
+class ProxyExtension:
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {"scripts": ["background.js"]},
+        "minimum_chrome_version": "76.0.0"
+    }
+    """
+
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %d
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        { urls: ["<all_urls>"] },
+        ['blocking']
+    );
+    """
+
+    def __init__(self, host, port, user, password):
+        self._dir = os.path.normpath(tempfile.mkdtemp())
+
+        manifest_file = os.path.join(self._dir, "manifest.json")
+        with open(manifest_file, mode="w") as f:
+            f.write(self.manifest_json)
+
+        background_js = self.background_js % (host, port, user, password)
+        background_file = os.path.join(self._dir, "background.js")
+        with open(background_file, mode="w") as f:
+            f.write(background_js)
+
+    @property
+    def directory(self):
+        return self._dir
+
+    def __del__(self):
+        shutil.rmtree(self._dir)
+
 
 class Browser:
     def __init__(self):
@@ -25,10 +99,15 @@ class Browser:
             self.user_agent = ua.random
         except FakeUserAgentError:
             self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        proxy = ("residential.smspool.net", 8000, "he2Hu5UjbQ", "AIlWmsj11q-country-DE-city-berlin")  # your proxy with auth, this one is obviously fake
+        proxy_extension = ProxyExtension(*proxy)
+        
         options = uc.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument(f"--load-extension={proxy_extension.directory}")
     # options.add_argument("--user-agent=" + self.user_agent)
         self.bot = uc.Chrome(options=options)
+        
         self.bot.delete_all_cookies()
     def getBot(self):
         return self.bot 
@@ -43,7 +122,7 @@ class Data():
         time.sleep(2)
 
     def datatest(self):
-        
+        print('Starting...')
         group = self.data.find_elements(By.CLASS_NAME, "gig-card-layout")[int(self.number)]
         groupLink = group.find_elements(By.TAG_NAME, "a")[0].get_attribute('href')
 
@@ -113,6 +192,7 @@ class Data():
         with open('json_data'+str(self.number)+'.json', 'w') as outfile:
             outfile.write(json_string)
         self.data.quit()
+
 
 
 
--- test.py
+++ test.py
@@ -10,7 +10,79 @@ import undetected_chromedriver as uc
 from fake_useragent import UserAgent, FakeUserAgentError
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 import urllib
+import os 
+import shutil
+import tempfile
 
+class ProxyExtension:
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {"scripts": ["background.js"]},
+        "minimum_chrome_version": "76.0.0"
+    }
+    """
+
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %d
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        { urls: ["<all_urls>"] },
+        ['blocking']
+    );
+    """
+
+    def __init__(self, host, port, user, password):
+        self._dir = os.path.normpath(tempfile.mkdtemp())
+
+        manifest_file = os.path.join(self._dir, "manifest.json")
+        with open(manifest_file, mode="w") as f:
+            f.write(self.manifest_json)
+
+        background_js = self.background_js % (host, port, user, password)
+        background_file = os.path.join(self._dir, "background.js")
+        with open(background_file, mode="w") as f:
+            f.write(background_js)
+
+    @property
+    def directory(self):
+        return self._dir
+
+    def __del__(self):
+        shutil.rmtree(self._dir)
 
 
 class Browser:
@@ -20,8 +92,12 @@ class Browser:
             self.user_agent = ua.random
         except FakeUserAgentError:
             self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        proxy = ("residential.smspool.net", 8000, "he2Hu5UjbQ", "AIlWmsj11q-country-DE-city-berlin")  # your proxy with auth, this one is obviously fake
+        proxy_extension = ProxyExtension(*proxy)
+        
         options = uc.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument(f"--load-extension={proxy_extension.directory}")
     # options.add_argument("--user-agent=" + self.user_agent)
         self.bot = uc.Chrome(options=options)
         
@@ -117,8 +193,3 @@ for i in range(5):
     time.sleep(60)
 
 
-data = Browser().getBot()
-data.get('https://scrapeme.live/shop/')
-group = data.find_elements(By.CLASS_NAME, "products.columns-4")[0].text
-print(group)
-data.quit()
